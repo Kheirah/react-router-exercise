d669012906b3282a9a99c350e673704fc7b15104
Ernst Stolz <eric@spiritsdontfly.com>
2024-07-04 17:27:02 +0200

    add sort by actor's name

--- src/Actors.jsx
+++ src/Actors.jsx
@@ -6,15 +6,17 @@ export default function Actors() {
     <div>
       <h3>Actors</h3>
       <ul>
-        {actors.map((actor) => (
-          <li key={actor.id}>
-            <Link to={`/actors/${actor.id}`}>
-              <h2>{actor.name}</h2>
-              <p>{actor.popularity}</p>
-              <img src={actor.image} alt={actor.title} />
-            </Link>
-          </li>
-        ))}
+        {actors
+          .sort((a, b) => (a.name < b.name ? -1 : 1))
+          .map((actor) => (
+            <li key={actor.id}>
+              <Link to={`/actors/${actor.id}`}>
+                <h2>{actor.name}</h2>
+                <p>{actor.popularity}</p>
+                <img src={actor.image} alt={actor.title} />
+              </Link>
+            </li>
+          ))}
       </ul>
     </div>
   );
