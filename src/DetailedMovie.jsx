6868e4701941986c3bc03fbfa24091889137e3f1
Ernst Stolz <eric@spiritsdontfly.com>
2024-07-04 12:02:32 +0200

    step 3: filter through actors to find the real names of the actors

--- src/DetailedMovie.jsx
+++ src/DetailedMovie.jsx
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom";
 import { movies } from "./data/movies";
+import { actors } from "./data/actors";
 import { useParams } from "react-router-dom";
 
 export default function DetailedMovies() {
   const { movieId } = useParams();
   const movie = movies.find((movie) => movie.id == movieId);
+  const actorsInMovie = actors.filter((actor) =>
+    movie.cast.find((cast) => cast.id == actor.id)
+  );
   return (
     <div>
       <h3>{movie.title}</h3>
       <img src={movie.image} alt={movie.title} />
       <ul>
         Cast:
-        {movie.cast.map((actor) => (
+        {actorsInMovie.map((actor) => (
           <li key={actor.id}>
             <Link to={`/actors/${actor.id}`}>
-              <h4>{actor.character}</h4>
+              <h4>{actor.name}</h4>
             </Link>
           </li>
         ))}
